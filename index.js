8efd76bf4bad85d572dc70c7b42b61d4571d4635
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2022-06-22 22:54:42 +0000

    complete v1 of the bot

--- index.js
+++ index.js
@@ -1,176 +1,187 @@
-const WikiTextParser = require('parse-wikitext');
-const wikiTextParser = new WikiTextParser('robloxgalaxy.wiki');
-const fetch = require('node-fetch');
-var nodemw = require('nodemw');
-const fs = require('fs');
-const chalk = require('chalk');
-const { promisify } = require('util');
+require('dotenv').config()
+
+const WikiTextParser = require('parse-wikitext')
+const wikiTextParser = new WikiTextParser('robloxgalaxy.wiki')
+const fetch = require('node-fetch')
+const NodeMW = require('nodemw')
+const chalk = require('chalk')
+const { promisify } = require('util')
+const cron = require('node-cron')
+const fs = require('fs/promises')
+const { performance } = require('perf_hooks')
 
 // Settings
-const verbose = process.env.verbose || false;
-const regex = /{{\s*Ship[ _]Infobox[ _]Template.*?}}/si;
-const disc_webhook = 'https://discord.com/api/webhooks/989011701619892285/224ie95dP5VRPH7eNHH56HzMN6iTc5i_tj7bI7_V33ACHhc5TFx8B8zr7lPlOUk1YuA8';
+const verbose = process.env.VERBOSE === 'true'
+const SHIP_INFOBOX_TEMPLATE_REGEX = /{{\s*Ship[ _]Infobox[ _]Template.*?}}/si
 
-var bot = new nodemw({
-	protocol: 'https',
-	server: 'robloxgalaxy.wiki',
-	path: '/',
-	debug: verbose,
-});
-
-const getArticle = promisify(bot.getArticle.bind(bot));
-const editArticle = promisify(bot.edit.bind(bot));
-const getArticleWikitext = promisify(wikiTextParser.getArticle.bind(wikiTextParser));
-
-// Fetch all ship data
-async function getAllShipData() {
-	const response = await fetch('https://galaxy.wingysam.xyz/api/v2/galaxypedia?token=aanLgKS6HW7fuZVM7khbFf8UfSV6knjT');
-
-	if (!response.ok) {
-		return null;
-	}
-
-	const data = await response.json();
-
-	return data;
+const SHIP_NAME_MAP = {
+  2018: '2018 Ship',
+  yname: 'Yname (ship)'
 }
 
-// Get an individual ships data
-async function getShipData(shipname) {
-	var shipdata;
+class GalaxypediaUpdater {
+  async main (username, password) {
+    this.bot = new NodeMW({
+      protocol: 'https',
+      server: 'robloxgalaxy.wiki',
+      path: '/',
+      debug: verbose
+    })
 
-	try {
-		shipdata = await getAllShipData();
-	} catch {
-		console.error(chalk.red('Seems like the host is down, trying to fetch from cache.'));
+    this.getArticle = promisify(this.bot.getArticle.bind(this.bot))
+    this.editArticle = promisify(this.bot.edit.bind(this.bot))
+    this.getArticleWikitext = promisify(wikiTextParser.getArticle.bind(wikiTextParser))
+    this.logIn = promisify(this.bot.logIn.bind(this.bot))
 
-		if (fs.existsSync('cache.json')) {
-			shipdata = JSON.parse(fs.readFileSync('cachejson'));
-		} else {
-			console.error(chalk.red('There is no cache file, unable to update!'));
-			return 'HostDown';
-		}
-	}
+    await this.logIn(username, password)
+    cron.schedule('0 * * * *', () => this.updateGalaxypediaShips())
+    this.updateGalaxypediaShips()
+  }
 
-	if (!shipdata[shipname]) return 33;
+  async updateGalaxypediaShips () {
+    try {
+      if (this.currentlyUpdating) {
+        console.log(`${chalk.redBright('[!]')} Already updating ships; not updating`)
+        return
+      }
+      this.currentlyUpdating = true
 
-	return shipdata[shipname];
+      this.shipsData = await this.getShipsData()
+      this.galaxypediaShipList = await this.getGalaxypediaShipList()
+      await this.updateShips()
+    } catch (error) {
+      console.error(error)
+    }
+
+    this.currentlyUpdating = false
+  }
+
+  async getShipsData () {
+    const response = await fetch(`https://galaxy.wingysam.xyz/api/v2/galaxypedia?token=${process.env.GALAXY_INFO_TOKEN}`)
+    if (!response.ok) throw new Error('Galaxy Info seems to be down.')
+    const galaxyInfoShips = await response.json()
+    return galaxyInfoShips
+  }
+
+  async getGalaxypediaShipList () {
+    const response = await fetch('https://robloxgalaxy.wiki/api.php?action=query&format=json&list=categorymembers&cmtitle=Category%3AShips&cmlimit=5000')
+    if (!response.ok) throw new Error('Galaxypedia appears to be down.')
+
+    const galaxypediaPageList = (await response.json()).query.categorymembers
+      .map(page => page.title)
+    const shipsList = galaxypediaPageList
+      .filter(pageName => !pageName.startsWith('Category:'))
+
+    return shipsList
+  }
+
+  async updateShips () {
+    for (const shipName of Object.keys(this.shipsData).sort()) {
+      await this.handleShip(this.shipsData[shipName])
+    }
+    console.log(chalk.greenBright('Complete!'))
+  }
+
+  async handleShip (ship) {
+    if (process.env.SHIP && ship.title !== process.env.SHIP) return
+    try {
+      console.log(`${chalk.yellow('Processing ')} ${chalk.cyanBright(ship.title)}...`)
+      const steps = await this.updateShip(ship)
+      const perf = verbose ? ` perf: ${steps.join(', ')}` : ''
+      console.log(`${chalk.green('Updated')} ${chalk.cyanBright(ship.title)}!` + perf)
+      await this.logChange(ship.title)
+    } catch (error) {
+      console.log(`${chalk.red('[!]')} ${chalk.cyanBright(ship.title)}: ${chalk.red(error.message)}`)
+    }
+  }
+
+  async updateShip (ship) {
+    const steps = []
+    async function step (name, prom) {
+      const start = performance.now()
+      const returned = await prom
+      const end = performance.now()
+      steps.push(`${name} ${(end - start).toFixed(2)}ms`)
+      return returned
+    }
+
+    const pageName = await step('getShipPageName', this.getShipPageName(ship))
+    const oldWikitext = await step('getArticle', this.getArticle(pageName))
+    if (!oldWikitext) throw new Error(`Wikitext for ${pageName} missing`)
+
+    const oldData = await step('parseWikiText', this.parseWikitext(oldWikitext))
+    const newData = await step('mergeData', this.mergeData(oldData, ship))
+
+    const newWikitext = await step('formatDataIntoWikitext', this.formatDataIntoWikitext(newData, oldWikitext))
+    if (newWikitext === oldWikitext) throw new Error('Already up-to-date')
+
+    await step('editArticle', this.editArticle(pageName, newWikitext, 'Automatic Infobox Update', false))
+    return steps
+  }
+
+  async getShipPageName (ship) {
+    if (this.galaxypediaShipList.includes(ship.title)) return ship.title
+    const mappedName = SHIP_NAME_MAP[ship.title]
+    if (mappedName && this.galaxypediaShipList.includes(mappedName)) return mappedName
+    throw new Error(`Can't find page name for ${ship.title}`)
+  }
+
+  async parseWikitext (wikitext) {
+    const matches = wikitext.match(SHIP_INFOBOX_TEMPLATE_REGEX)
+    if (!matches) throw new Error('Could not find infobox wikitext')
+
+    const data = wikiTextParser.parseTemplate(matches[0]).namedParts
+    if (data.image1 && data.image1.startsWith('<gallery')) data.image1 = wikitext.match(/<gallery.*?>.*?<\/gallery>/sg)[0]
+
+    if (verbose) console.log('Ship Data Raw\n' + JSON.stringify(data, null, '\t'))
+
+    return data
+  }
+
+  async mergeData (...objects) {
+    const data = {}
+    function mergeObjectIn (obj) {
+      for (const key of Object.keys(obj)) {
+        if (obj[key] === '') continue
+        data[key] = obj[key]
+      }
+    }
+
+    for (const obj of objects) {
+      mergeObjectIn(obj)
+    }
+
+    if (verbose) console.log('Ship Data Merged: ', { objects, data })
+    return data
+  }
+
+  async formatDataIntoWikitext (data, oldWikitext) {
+    const newWikitext = oldWikitext.replace(SHIP_INFOBOX_TEMPLATE_REGEX, '{{Ship Infobox Template\n|' + Object.entries(data).map(([key, val]) => `${key} = ${val}`).join('\n|') + '\n}}')
+
+    if (verbose) {
+      console.log(chalk.blueBright('------------ OLD PAGE WIKITEXT ------------'))
+      console.log(oldWikitext)
+      console.log(chalk.blueBright('------------ NEW PAGE WIKITEXT ------------'))
+      console.log(newWikitext)
+    }
+    return newWikitext
+  }
+
+  async logChange (shipName) {
+    await fetch(process.env.WEBHOOK, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: `Updated **${shipName}**!`
+      })
+    })
+  }
 }
 
-async function processShip(shipname, wikitext) {
-	const shipdata = await getShipData(shipname);
-	if (shipdata == 33) return 33;
-	const matches = wikitext.match(regex);
-	if (!matches) {
-		console.log('No infobox found!');
-		return 23; // No infoboxes found
-	}
-
-	const data = wikiTextParser.parseTemplate(matches[0]).namedParts;
-	if (data.image1 && data.image1.startsWith('<gallery')) data.image1 = wikitext.match(/<gallery.*?>.*?<\/gallery>/sg)[0];
-
-	if (verbose) console.log('Ship Data Raw\n' + JSON.stringify(data, null, '\t'));
-
-	for (const dataval of Object.keys(shipdata)) {
-		data[dataval] = shipdata[dataval];
-	}
-
-	Object.keys(data).forEach(key => { // Remove blank keys
-		if (data[key] === '') delete data[key];
-	});
-
-	if (verbose) console.log('Ship Data Collected from API\n' + JSON.stringify(shipdata, null, '\t'));
-	if (verbose) console.log('Ship Data Merged\n' + JSON.stringify(data, null, '\t'));
-
-	const newCode = wikitext.replace(regex, '{{Ship Infobox Template\n|' + Object.entries(data).map(([key, val]) => {
-		if (key.includes('turret')) {
-			return `${key} = ${val.toString().replaceAll('\n', '\n\n')}`;
-		} else {
-			return `${key} = ${val}`;
-		}
-	}).join('\n|') + '\n}}');
-	if (newCode === wikitext) {
-		console.log(chalk.green(`${shipname} is up to date!`));
-		return 22; // Up to date
-	}
-
-	if (verbose) {
-		console.log(chalk.blueBright('------------ OLD PAGE WIKITEXT ------------'));
-		console.log(wikitext);
-		console.log(chalk.blueBright('------------ NEW PAGE WIKITEXT ------------'));
-		console.log(newCode);
-	}
-
-	console.log(chalk.yellow('Processed ') + chalk.cyanBright(`${shipname}!`));
-
-	return newCode;
-}
-
-async function update(ship) {
-	const article = await getArticleWikitext(ship);
-
-	const processed = await processShip(ship, article);
-
-	if (processed == 23 || processed == 22 || processed == 33) return console.warn(chalk.red(`Skipped updating ${ship} due to status code override ${processed}!`));
-
-	await editArticle(ship, processed, 'Automatic Infobox Update', false);
-
-	console.log(chalk.green(`Updated ${ship}!`));
-	fetch(disc_webhook, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({
-		'content': `Updated ${ship}!`,
-	}) });
-
-	return processed;
-}
-
-async function prepare(ship) {
-	const article = await getArticle(ship);
-	if (article) {
-		console.log(chalk.yellow(`${ship} exists!`));
-		await update(ship);
-	} else {
-		console.log(chalk.yellow(`${ship} does not exist!`));
-	}
-}
-
-bot.logIn('Ketchupbot101', 'Small-Bot123', async (err) => {
-	if (err) return err;
-
-	const thankuyname = await getAllShipData();
-
-	// At every hour, cache the ship info to use in case it is down. But also run it once during startup.
-	try {
-		fs.writeFileSync('cache.json', JSON.stringify(thankuyname, null, '\t'));
-		console.log(chalk.blueBright('Cached ship info'));
-	} catch {
-		console.log(chalk.red('Seems like the host is down!'));
-	}
-	setInterval(async () => {
-		const rn = new Date().getMinutes();
-
-		if (rn === '00') {
-			try {
-				fs.writeFileSync('cache.json', JSON.stringify(await getAllShipData(), null, '\t'));
-				console.log(chalk.blueBright('Cached ship info'));
-			} catch {
-				console.log(chalk.red('Seems like the host is down!'));
-			}
-		}
-
-	}, 1000);
-
-	const allshipsraw = await fetch('https://robloxgalaxy.wiki/api.php?action=query&format=json&list=categorymembers&cmtitle=Category%3AShips&cmlimit=5000').then(response => response.json());
-
-	const shipsfiltered = Object.entries(allshipsraw.query.categorymembers).filter(key => {
-		const title = key[1].title;
-		if (title.toString().startsWith('Category:')) return false;
-		return key[1].title;
-	});
-
-	const ships = shipsfiltered.map(e => e[1].title);
-
-	for (const ship of ships) {
-		await prepare(ship.toString());
-	}
-	console.log(chalk.greenBright('Complete!'));
-});
+;(async () => {
+  console.log((await fs.readFile('banner.txt')).toString())
+  const galaxypediaUpdater = new GalaxypediaUpdater()
+  await galaxypediaUpdater.main(process.env.MW_LOGIN, process.env.MW_PASS)
+})()

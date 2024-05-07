41ff172fbedcc43d5dfd29a18206435d44226b31
smallketchup82 <69545310+smallketchup82@users.noreply.github.com>
2024-05-07 09:14:13 -0400

    Add NodeMWP

--- src/NodeMWP.ts
+++ src/NodeMWP.ts
@@ -1,8 +1,29 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import Bot from "nodemw";
+import type { BotOptions } from "nodemw/lib/types.js";
+import { promisify } from "util";
+
 /**
  * NodeMW Promisified
- * 
+ *
  * Wrapper for the NodeMW library which uses promises instead of callbacks
  */
-export default class NodeMWP {
+export default class NodeMWP extends Bot {
+    constructor(params: string | BotOptions) {
+        super(params);
+    }
 
+    public getArticle = promisify(super.getArticle.bind(this));
+
+    public edit = promisify(super.edit.bind(this));
+
+    public parse = promisify(super.parse.bind(this));
+
+    public getArticleRevisions = promisify(
+        super.getArticleRevisions.bind(this),
+    );
+
+    public logIn = promisify(super.logIn.bind(this));
+
+    public getPagesInCategory = promisify(super.getPagesInCategory.bind(this));
 }
